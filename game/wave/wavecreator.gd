7872084321a7f678570c98580cd430a7ba82906e
Marcelo Vasques <msouvas@gmail.com>
2017-01-21 11:00:59 -0200

    Waves increase speed

--- game/wave/wavecreator.gd
+++ game/wave/wavecreator.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 var wave
-export var timespawn = 10
+export var timespawn = 5
+export var timetohard = 20
 var acctime = 0
+var totaltime = 0
 var height
 
 func _ready():
@@ -12,8 +14,12 @@ func _ready():
 
 func _fixed_process(delta):
 	acctime += delta
+	totaltime += delta
+	if(totaltime > timetohard && timespawn > 2):
+		timespawn = timespawn / 2
+		totaltime = 0
 	if(acctime > timespawn):
 		var newWave = wave.instance()
-		newWave.set_pos(Vector2(50, rand_range(0, height)))
+		newWave.set_pos(Vector2(50, rand_range(0.2*height, 0.8*height)))
 		get_tree().get_root().add_child(newWave)
 		acctime = 0
