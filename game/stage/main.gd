4ad992e81f2811873ada40c42e7e56445f54f206
Marcelo Vasques <msouvas@gmail.com>
2017-01-21 19:48:25 -0200

    Kernels disappear

--- game/stage/main.gd
+++ game/stage/main.gd
@@ -1,15 +1,18 @@
 extends Node
 
 func _ready():
-	# Pause
+	hideKernels()
+	
+	get_node("menu/popcorn").connect("microwave_start", self, "on_start")
+	
+func hideKernels():
+		# Pause
 	get_tree().set_pause(true)
 	
 	# Hide kernels
 	var kernels = get_node("corns").get_children()
 	for kernel in kernels:
 		kernel.set_hidden(true)
-	
-	get_node("menu/popcorn").connect("microwave_start", self, "on_start")
 
 func on_start():
 	# Unpause
@@ -42,15 +45,22 @@ func on_game_over():
 	get_node("menu/credits").set_disabled(false)
 	
 	# Reload kernels
-	get_node("corns").queue_free()
+	get_node("corns").free()
 	var nodes_res = preload("res://corn/corn_collection.tscn")
 	var node_collection = nodes_res.instance()
 	node_collection.set_name("corns")
 	add_child(node_collection)
 	
 	get_node("player_corn").queue_free()
-	call_deferred("add_child", load("res://corn/player_corn.tscn").instance())
+	var player_res = preload("res://corn/player_corn.tscn")
+	var player_instance = player_res.instance()
+	player_instance.set_name("player_corn")
+	add_child(player_instance)
+	player_instance.set_global_pos(Vector2(121.922997, 282.2609))
 	
 	# Reveal other elements
 	get_node("menu/quit/door").show()
 	get_node("menu/Sprite").show()
+	
+	# Hide again
+	hideKernels()
