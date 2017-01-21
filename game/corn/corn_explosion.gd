f9f0a1a4d5f76676003d5451093d94ae42fcc031
Marcelo Vasques <msouvas@gmail.com>
2017-01-21 04:32:12 -0200

    Explosion physics seem good

--- game/corn/corn_explosion.gd
+++ game/corn/corn_explosion.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 #export(float)
-var explosion_power = 50.0
+var explosion_power = 10000
 var radius
 
 func _ready():
@@ -12,8 +12,8 @@ func explode():
 	var bodies_inside = get_overlapping_bodies()
 	for body in bodies_inside:
 		if(body extends RigidBody2D):
-			var vec = body.get_pos() - get_pos()
-			#vec = vec.normalized()
-			var impulse = vec.normalized() * explosion_power * (vec.length() / radius)
+			var vec = body.get_global_pos() - get_global_pos()
+			# vec = vec.normalized()
+			var impulse = vec.normalized() * explosion_power * (1 - vec.length() / radius)
 			body.apply_impulse(Vector2(0, 0), impulse)	
-			body.queue_free()
+			#body.queue_free()
