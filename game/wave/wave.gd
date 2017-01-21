f1f0501e4a25cdb0334378306c2d8b6887ca052c
Marcelo Vasques <msouvas@gmail.com>
2017-01-21 15:20:45 -0200

    Left wave bug solved

--- game/wave/wave.gd
+++ game/wave/wave.gd
@@ -2,15 +2,17 @@ extends KinematicBody2D
 
 export(int) var speed = 500
 export var left = false
+var acclast = 0
 
 func _ready():
 	set_fixed_process(true)
 	if(left):
-		move(Vector2(-0.01*rand_range(0, 100), 0.01*rand_range(0, 100)))
+		move(Vector2(-0.01*rand_range(20, 100), 0.01*rand_range(0, 100)))
 	else:
-		move(Vector2(0.01*rand_range(0, 100), 0.01*rand_range(0, 100)))
+		move(Vector2(0.01*rand_range(20, 100), 0.01*rand_range(0, 100)))
 	
 func _fixed_process(delta):
+	acclast += delta
 	var move_dir = changeDir().normalized()
 	if(left):
 		set_rot(Vector2(-1, 0).angle_to(get_travel()))
@@ -20,11 +22,9 @@ func _fixed_process(delta):
 	move(motion)
 	
 func changeDir():
-	if is_colliding():
+	if is_colliding() :
+		acclast = 0
+		print(get_collision_normal().reflect(get_travel()))
 		return get_collision_normal().reflect(get_travel())
 	else:
 		return get_travel()
-
-func moveLeft():
-	left = true
-	move(Vector2(-0.01*rand_range(0, 100), 0.01*rand_range(0, 100)))
