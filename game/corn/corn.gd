a7eec30d84c46dd488469bc0dcf92f1adb98a09a
Khin Baptista <khin.baptista@gmail.com>
2017-01-21 01:27:55 -0200

    Fixed jump and added corn color modulation

--- game/corn/corn.gd
+++ game/corn/corn.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-export(int, 0, 1000) var health = 100
+export(float, 0, 1000, 0.5) var health = 100.0
 export(Color) var start_color
 export(Color) var pop_color
 
@@ -13,6 +13,7 @@ func _ready():
 
 func hit(damage):
 	health -= damage
+	get_node("Sprite").set_modulate(start_color.linear_interpolate(pop_color, (max_health - health) / max_health))
 	
 	if health <= 0:
 		health = 0
